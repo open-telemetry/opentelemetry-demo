7e9b57d429683e9879a98dcced2aea4bd2775816
Tristan Sloughter <t@crashfast.com>
2022-05-24 14:59:23 -0600

    add featureflag service (Erlang/Elixir)
    HTTP interface is on port FEATURE_FLAG_SERVICE_PORT and
    the grpc service is run on FEATURE_FLAG_GRPC_SERVICE_PORT.
    
    Creating and updating flags is done through the web interface.
    The GRPC service only provides fetching of a flag by name.

---
+++ src/featureflagservice/test/support/fixtures/feature_flags_fixtures.ex
@@ -0,0 +1,27 @@
+defmodule Featureflagservice.FeatureFlagsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Featureflagservice.FeatureFlags` context.
+  """
+
+  @doc """
+  Generate a unique feature_flag name.
+  """
+  def unique_feature_flag_name, do: "some name#{System.unique_integer([:positive])}"
+
+  @doc """
+  Generate a feature_flag.
+  """
+  def feature_flag_fixture(attrs \\ %{}) do
+    {:ok, feature_flag} =
+      attrs
+      |> Enum.into(%{
+        description: "some description",
+        enabled: true,
+        name: unique_feature_flag_name()
+      })
+      |> Featureflagservice.FeatureFlags.create_feature_flag()
+
+    feature_flag
+  end
+end
