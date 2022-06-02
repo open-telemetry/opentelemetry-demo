0313a696eb83df7a87c6538c7233708a1fa05c50
Michael Maxwell <micmax@microsoft.com>
2022-06-02 10:34:15 -0700

    hardcode addr

--- src/recommendationservice/client.py
+++ src/recommendationservice/client.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 import grpc
 import demo_pb2
 import demo_pb2_grpc
@@ -24,18 +23,13 @@ from logger import getJSONLogger
 logger = getJSONLogger('recommendationservice-server')
 
 if __name__ == "__main__":
-    # get port
-    if len(sys.argv) > 1:
-        port = sys.argv[1]
-    else:
-        port = "8080"
-
     # set up server stub
-    channel = grpc.insecure_channel('localhost:'+port)
-    
+    channel = grpc.insecure_channel('localhost:8080')
     stub = demo_pb2_grpc.RecommendationServiceStub(channel)
+
     # form request
     request = demo_pb2.ListRecommendationsRequest(user_id="test", product_ids=["test"])
+
     # make call to server
     response = stub.ListRecommendations(request)
     logger.info(response)
