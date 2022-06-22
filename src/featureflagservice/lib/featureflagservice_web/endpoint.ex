71913b0d92773998ab537b94bead9898c81cc505
Maciej Szlosarczyk <maciej@sosek.net>
2022-06-22 22:15:28 +0300

    Cleanup FeatureFlagService code
    * Remove unused deps (mailer, live dashboard)
    * Clean up compilation warnings
    * Remove unused code
    * Rename function to more Elixir-like
    Featureflagservice.FeatureFlags.list_feature_flags/0
    * Remove phoenix branding from UI
    * Add buttons and nicer links

--- src/featureflagservice/lib/featureflagservice_web/endpoint.ex
+++ src/featureflagservice/lib/featureflagservice_web/endpoint.ex
@@ -31,10 +31,6 @@ defmodule FeatureflagserviceWeb.Endpoint do
     plug Phoenix.Ecto.CheckRepoStatus, otp_app: :featureflagservice
   end
 
-  plug Phoenix.LiveDashboard.RequestLogger,
-    param_key: "request_logger",
-    cookie_key: "request_logger"
-
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
--- src/featureflagservice/test/support/data_case.ex
+++ src/featureflagservice/test/support/data_case.ex
@@ -36,7 +36,9 @@ defmodule Featureflagservice.DataCase do
   Sets up the sandbox based on the test tags.
   """
   def setup_sandbox(tags) do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Featureflagservice.Repo, shared: not tags[:async])
+    pid =
+      Ecto.Adapters.SQL.Sandbox.start_owner!(Featureflagservice.Repo, shared: not tags[:async])
+
     on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
   end
 
