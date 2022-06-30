b92faa57bd82d1a9b5b1d78cbc3852f619878e0c
Pierre Tessier <pierre@pierretessier.com>
2022-06-29 19:35:31 -0700

    Update src/checkoutservice/main.go
    Co-authored-by: Michael Maxwell <mike.ian.maxwell@gmail.com>

--- src/checkoutservice/main.go
+++ src/checkoutservice/main.go
@@ -251,7 +251,7 @@ func (cs *checkoutService) prepareOrderItemsAndShippingQuoteFromCart(ctx context
 	out.cartItems = cartItems
 	out.orderItems = orderItems
 
-	shippingCostFloat, _ := strconv.ParseFloat(fmt.Sprintf("%d.%02d", shippingPrice.GetUnits(), shippingPrice.GetNanos()/10000000), 64)
+	shippingCostFloat, _ := strconv.ParseFloat(fmt.Sprintf("%d.%02d", shippingPrice.GetUnits(), shippingPrice.GetNanos()/1000000000), 64)
 
 	span.SetAttributes(
 		attribute.Float64("app.order.shipping.cost", shippingCostFloat),
