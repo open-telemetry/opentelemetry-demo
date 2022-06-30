012ee1983fefd82b443d40e884ddc8643be9a3e9
Pierre Tessier <pierre@pierretessier.com>
2022-06-29 19:35:23 -0700

    Update src/checkoutservice/main.go
    Co-authored-by: Michael Maxwell <mike.ian.maxwell@gmail.com>

--- src/checkoutservice/main.go
+++ src/checkoutservice/main.go
@@ -199,7 +199,7 @@ func (cs *checkoutService) PlaceOrder(ctx context.Context, req *pb.PlaceOrderReq
 	}
 
 	shippingCostFloat, _ := strconv.ParseFloat(fmt.Sprintf("%d.%02d", prep.shippingCostLocalized.GetUnits(), prep.shippingCostLocalized.GetNanos()/1000000000), 64)
-	totalPriceFloat, _ := strconv.ParseFloat(fmt.Sprintf("%d.%02d", total.GetUnits(), total.GetNanos()/10000000), 64)
+	totalPriceFloat, _ := strconv.ParseFloat(fmt.Sprintf("%d.%02d", total.GetUnits(), total.GetNanos()/1000000000), 64)
 
 	span.SetAttributes(
 		attribute.String("app.order.id", orderID.String()),
