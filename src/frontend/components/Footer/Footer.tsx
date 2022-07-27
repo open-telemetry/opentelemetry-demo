3a7b71f6b5bc3b89cabdf37d4c0dbd5c792bd388
xoscar <oscar-rreyes1@hotmail.com>
2022-07-27 10:24:35 -0500

    39 removing the deployment details

--- src/frontend/components/Footer/Footer.tsx
+++ src/frontend/components/Footer/Footer.tsx
@@ -1,13 +1,7 @@
+import SessionGateway from '../../gateways/Session.gateway';
 import * as S from './Footer.styled';
 
-const sessionId = '123';
-const requestId = '1234';
-
-const deploymentDetails = {
-  pod: '123',
-  zone: '',
-  clusterName: '',
-};
+const { userId: sessionId } = SessionGateway.getSession();
 
 const Footer = () => {
   return (
@@ -15,18 +9,7 @@ const Footer = () => {
       <div>
         <p>This website is hosted for demo purpose only. It is not an actual shop. This is not a Google product</p>
         <p>
-          <span>session-id: {sessionId}</span> - <span>request-id: {requestId}</span>
-          <br />
-          <b>Cluster: </b>
-          {deploymentDetails.clusterName}
-          <br />
-          <b>Zone: </b>
-          {deploymentDetails.zone}
-          <br />
-          <b>Pod: </b>
-          {deploymentDetails.pod}
-          <br />
-          Deployment details are still loading. Try refreshing this page.
+          <span>session-id: {sessionId}</span>
         </p>
       </div>
       <p>
