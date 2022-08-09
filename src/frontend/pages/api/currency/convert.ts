d1687904f052e3694ab21a2dcda35ed151b5c4e7
Oscar Reyes <oscar-rreyes1@hotmail.com>
2022-08-09 17:17:06 -0500

    39 Moving the Front end application from Go to Next.js (#236)

---
+++ src/frontend/pages/api/currency/convert.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import CurrencyGateway from '../../../gateways/rpc/Currency.gateway';
+import { CurrencyConversionRequest, Money } from '../../../protos/demo';
+
+type TResponse = Money;
+
+const handler = async ({ method, body }: NextApiRequest, res: NextApiResponse<TResponse>) => {
+  switch (method) {
+    case 'POST': {
+      const { from, toCode } = body as CurrencyConversionRequest;
+
+      const result = await CurrencyGateway.convert(from!, toCode);
+
+      return res.status(200).json(result);
+    }
+
+    default: {
+      return res.status(405);
+    }
+  }
+};
+
+export default handler;
---
+++ src/frontend/styles/Home.styled.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 0 20px;
+
+  ${({ theme }) => theme.breakpoints.desktop} {
+    padding: 0 100px;
+  }
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  ${({ theme }) => theme.breakpoints.desktop} {
+    margin-top: 100px;
+  }
+`;
+
+export const HotProducts = styled.div`
+  margin-bottom: 20px;
+
+  ${({ theme }) => theme.breakpoints.desktop} {
+    margin-bottom: 100px;
+  }
+`;
+
+export const HotProductsTitle = styled.h1`
+  font-size: ${({ theme }) => theme.sizes.mLarge};
+  font-weight: ${({ theme }) => theme.fonts.bold};
+
+  ${({ theme }) => theme.breakpoints.desktop} {
+    font-size: ${({ theme }) => theme.sizes.dxLarge};
+  }
+`;
+
+export const Home = styled.div`
+  @media (max-width: 992px) {
+    ${Content} {
+      width: 100%;
+    }
+  }
+`;
