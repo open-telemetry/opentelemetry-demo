c35079021544410854b3858c13c7b0fb66ba4abc
xoscar <oscar-rreyes1@hotmail.com>
2022-08-12 09:26:35 -0500

    backend instrumentation cleanup

--- src/frontend/utils/telemetry/Instrumentation.js
+++ src/frontend/utils/telemetry/Instrumentation.js
@@ -1,28 +1,21 @@
 const { CompositePropagator, W3CBaggagePropagator, W3CTraceContextPropagator } = require('@opentelemetry/core');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
-const { Resource } = require('@opentelemetry/resources');
-const { BasicTracerProvider, SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+const { NodeSDK, api } = require('@opentelemetry/sdk-node');
 
-const exporter = new OTLPTraceExporter({
-  url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
-});
-
-const provider = new BasicTracerProvider({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: process.env.OTEL_SERVICE_NAME,
-  }),
-});
-
-provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-
-provider.register({
-  propagator: new CompositePropagator({
+api.propagation.setGlobalPropagator(
+  new CompositePropagator({
     propagators: [new W3CBaggagePropagator(), new W3CTraceContextPropagator()],
-  }),
+  })
+);
+
+const sdk = new NodeSDK({
+  traceExporter: new OTLPTraceExporter(),
+  instrumentations: [getNodeAutoInstrumentations()],
 });
+
+sdk.start();
 
 registerInstrumentations({
   instrumentations: [getNodeAutoInstrumentations()],
