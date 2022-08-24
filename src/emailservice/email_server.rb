df949fcc5adb6d6a78cdb72056f946e80825713e
Pierre Tessier <pierre@pierretessier.com>
2022-08-24 01:45:36 -0400

    remove email body from log output (#341)

--- src/emailservice/email_server.rb
+++ src/emailservice/email_server.rb
@@ -38,9 +38,10 @@ def send_email(data)
       from:     "noreply@example.com",
       subject:  "Your confirmation email",
       body:     erb(:confirmation, locals: { order: data.order }),
-      via:      :logger
+      via:      :test
     )
     span.set_attribute("app.email.recipient", data.email)
+    puts "Order confirmation email sent to: #{data.email}"
   end
   # manually created spans need to be ended
   # in Ruby, the method `in_span` ends it automatically
