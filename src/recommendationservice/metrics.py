34aec060e5c0fc99563b77dd2c3cd55ee7d9737a
Adriana Villela <indycomp0t1@gmail.com>
2022-10-12 12:47:41 -0400

    Fix per PR suggestions

--- src/recommendationservice/metrics.py
+++ src/recommendationservice/metrics.py
@@ -4,57 +4,57 @@ from gc import callbacks
 from typing import Iterable
 import psutil
 
-from opentelemetry.metrics import (
-    CallbackOptions,
-    Observation,
-)
+# from opentelemetry.metrics import (
+#     CallbackOptions,
+#     Observation,
+# )
 
 # RAM usage
-def ram_usage_callback(options: CallbackOptions) -> Iterable[Observation]:
-    observations = []    
-    ram_percent = psutil.virtual_memory().percent
-    print(f"ram_percent: {ram_percent}")
-    labels = {"dimension": "value"}
-    observations.append(Observation(ram_percent, labels))
+# def ram_usage_callback(options: CallbackOptions) -> Iterable[Observation]:
+#     observations = []    
+#     ram_percent = psutil.virtual_memory().percent
+#     print(f"ram_percent: {ram_percent}")
+#     labels = {"dimension": "value"}
+#     observations.append(Observation(ram_percent, labels))
     
-    return observations
+#     return observations
 
-# CPU Time callback
-def cpu_time_callback(options: CallbackOptions) -> Iterable[Observation]:
-    observations = []
-    with open("/proc/stat") as procstat:
-        procstat.readline()  # skip the first line
-        for line in procstat:
-            if not line.startswith("cpu"): break
-            cpu, *states = line.split()
-            observations.append(Observation(int(states[0]) // 100, {"cpu": cpu, "state": "user"}))
-            observations.append(Observation(int(states[1]) // 100, {"cpu": cpu, "state": "nice"}))
-            observations.append(Observation(int(states[2]) // 100, {"cpu": cpu, "state": "system"}))
-            # ... other states
-    return observations
+# # CPU Time callback
+# def cpu_time_callback(options: CallbackOptions) -> Iterable[Observation]:
+#     observations = []
+#     with open("/proc/stat") as procstat:
+#         procstat.readline()  # skip the first line
+#         for line in procstat:
+#             if not line.startswith("cpu"): break
+#             cpu, *states = line.split()
+#             observations.append(Observation(int(states[0]) // 100, {"cpu": cpu, "state": "user"}))
+#             observations.append(Observation(int(states[1]) // 100, {"cpu": cpu, "state": "nice"}))
+#             observations.append(Observation(int(states[2]) // 100, {"cpu": cpu, "state": "system"}))
+#             # ... other states
+#     return observations
 
-# CPU usage
-def cpu_usage_callback(options: CallbackOptions) -> Iterable[Observation]:
-    observations = []    
-    for (number, percent) in enumerate(psutil.cpu_percent(percpu=True)):
-        print(f"cpu_number: {number}, cpu_percent: {percent}")
-        labels = {"cpu_number": str(number)}
-        observations.append(Observation(percent, labels))
+# # CPU usage
+# def cpu_usage_callback(options: CallbackOptions) -> Iterable[Observation]:
+#     observations = []    
+#     for (number, percent) in enumerate(psutil.cpu_percent(percpu=True)):
+#         print(f"cpu_number: {number}, cpu_percent: {percent}")
+#         labels = {"cpu_number": str(number)}
+#         observations.append(Observation(percent, labels))
         
-    return observations
+#     return observations
         
-def observable_counter_callback(options: CallbackOptions) -> Iterable[Observation]:
-    yield Observation(1, {})
+# def observable_counter_callback(options: CallbackOptions) -> Iterable[Observation]:
+#     yield Observation(1, {})
 
 
-def observable_up_down_counter_callback(
-    options: CallbackOptions,
-) -> Iterable[Observation]:
-    yield Observation(-10, {})
+# def observable_up_down_counter_callback(
+#     options: CallbackOptions,
+# ) -> Iterable[Observation]:
+#     yield Observation(-10, {})
 
 
-def observable_gauge_callback(options: CallbackOptions) -> Iterable[Observation]:
-    yield Observation(9, {})
+# def observable_gauge_callback(options: CallbackOptions) -> Iterable[Observation]:
+#     yield Observation(9, {})
 
 
 def init_metrics(meter):
@@ -72,20 +72,20 @@ def init_metrics(meter):
     )
 
     # CPU usage
-    cpu_usage = meter.create_observable_counter(
-        "cpu_usage",
-        callbacks=[cpu_usage_callback],
-        unit="%",
-        description="CPU usage"
-    )
+    # cpu_usage = meter.create_observable_counter(
+    #     "cpu_usage",
+    #     callbacks=[cpu_usage_callback],
+    #     unit="%",
+    #     description="CPU usage"
+    # )
 
     # RAM usage
-    ram_usage = meter.create_observable_up_down_counter(
-        "ram_usage",
-        callbacks=[ram_usage_callback],
-        unit="1",
-        description="RAM usage"        
-    )
+    # ram_usage = meter.create_observable_up_down_counter(
+    #     "ram_usage",
+    #     callbacks=[ram_usage_callback],
+    #     unit="1",
+    #     description="RAM usage"        
+    # )
     
     attributes = {"application.name": "otel-demo"}
     
@@ -93,8 +93,8 @@ def init_metrics(meter):
         "list_recommendations_request_counter": list_recommendations_request_counter,
         "attributes": attributes,
         "app_recommendations_counter": app_recommendations_counter,
-        "cpu_usage": cpu_usage,
-        "ram_usage": ram_usage
+        # "cpu_usage": cpu_usage,
+        # "ram_usage": ram_usage
     }
     
     return rec_svc_metrics
