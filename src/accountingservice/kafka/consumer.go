a975e1f212dbbe2ded1ea31cdbadf7effeb8241e
secustor <sebastian@poxhofer.at>
2022-10-17 09:39:32 +0200

    feat: add Accountingservice which consumes orders from Kafka

---
+++ src/accountingservice/kafka/consumer.go
@@ -0,0 +1,64 @@
+package kafka
+
+import (
+	"context"
+	"github.com/Shopify/sarama"
+	"github.com/sirupsen/logrus"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/Shopify/sarama/otelsarama"
+	"log"
+)
+
+var (
+	Topic           = "orders"
+	ProtocolVersion = sarama.V3_0_0_0
+	GroupID         = "accountingservice"
+)
+
+func StartConsumerGroup(ctx context.Context, brokers []string, log *logrus.Logger) error {
+	saramaConfig := sarama.NewConfig()
+	saramaConfig.Version = ProtocolVersion
+	// So we can know the partition and offset of messages.
+	saramaConfig.Producer.Return.Successes = true
+
+	consumerGroup, err := sarama.NewConsumerGroup(brokers, GroupID, saramaConfig)
+	if err != nil {
+		return err
+	}
+
+	// Wrap instrumentation
+	handler := groupHandler{
+		log: log,
+	}
+	wrappedHandler := otelsarama.WrapConsumerGroupHandler(&handler)
+
+	err = consumerGroup.Consume(ctx, []string{Topic}, wrappedHandler)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type groupHandler struct {
+	log *logrus.Logger
+}
+
+func (g *groupHandler) Setup(_ sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (g *groupHandler) Cleanup(_ sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (g *groupHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for {
+		select {
+		case message := <-claim.Messages():
+			log.Printf("Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
+			session.MarkMessage(message, "")
+
+		case <-session.Context().Done():
+			return nil
+		}
+	}
+}
