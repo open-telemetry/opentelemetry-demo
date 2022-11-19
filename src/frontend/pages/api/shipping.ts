46ffb403044dd0e4b7ccaeff38c9bce9473ae4e6
Pierre Tessier <pierre@pierretessier.com>
2022-11-18 23:29:44 -0500

    add address to shipping api call

--- src/frontend/pages/api/shipping.ts
+++ src/frontend/pages/api/shipping.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import InstrumentationMiddleware from '../../utils/telemetry/InstrumentationMiddleware';
 import ShippingGateway from '../../gateways/rpc/Shipping.gateway';
-import { CartItem, Empty, Money } from '../../protos/demo';
+import { Address, CartItem, Empty, Money } from '../../protos/demo';
 import CurrencyGateway from '../../gateways/rpc/Currency.gateway';
 
 type TResponse = Money | Empty;
@@ -10,7 +10,8 @@ const handler = async ({ method, query }: NextApiRequest, res: NextApiResponse<T
   switch (method) {
     case 'GET': {
       const { itemList = '', currencyCode = 'USD', address = '' } = query;
-      const { costUsd } = await ShippingGateway.getShippingCost(JSON.parse(itemList as string, JSON.parse(address as string)) as CartItem[]);
+      const { costUsd } = await ShippingGateway.getShippingCost(JSON.parse(itemList as string) as CartItem[],
+          JSON.parse(address as string) as Address);
       const cost = await CurrencyGateway.convert(costUsd!, currencyCode as string);
 
       return res.status(200).json(cost!);
