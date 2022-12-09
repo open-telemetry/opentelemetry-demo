1c16770450132983fe9d159fc237ca7ead95b530
Pierre Tessier <pierre@pierretessier.com>
2022-12-08 21:15:50 -0500

    remove reqwest tracing (revert 610)
    Signed-off-by: Pierre Tessier <pierre@pierretessier.com>

--- src/shippingservice/src/main.rs
+++ src/shippingservice/src/main.rs
@@ -29,8 +29,8 @@ use opentelemetry_otlp::{self, WithExportConfig};
 
 use tonic::transport::Server;
 
-use tracing_subscriber::Registry;
-use tracing_subscriber::layer::SubscriberExt;
+// use tracing_subscriber::Registry;
+// use tracing_subscriber::layer::SubscriberExt;
 
 use log::*;
 use simplelog::*;
@@ -77,11 +77,11 @@ fn init_tracer() -> Result<sdktrace::Tracer, TraceError> {
         .install_batch(opentelemetry::runtime::Tokio)
 }
 
-fn init_reqwest_tracing(tracer: sdktrace::Tracer) -> Result<(), tracing::subscriber::SetGlobalDefaultError> {
-    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
-    let subscriber = Registry::default().with(telemetry);
-    tracing::subscriber::set_global_default(subscriber)
-}
+//fn init_reqwest_tracing(tracer: sdktrace::Tracer) -> Result<(), tracing::subscriber::SetGlobalDefaultError> {
+//    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+//    let subscriber = Registry::default().with(telemetry);
+//    tracing::subscriber::set_global_default(subscriber)
+//}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -91,7 +91,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await;
 
     init_logger()?;
-    init_reqwest_tracing(init_tracer()?)?;
+    init_tracer()?;
+    //init_reqwest_tracing(init_tracer()?)?;
     info!("OTel pipeline created");
     let port = env::var("SHIPPING_SERVICE_PORT").expect("$SHIPPING_SERVICE_PORT is not set");
     let addr = format!("0.0.0.0:{}", port).parse()?;
