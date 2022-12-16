e965392e076d3e2087d7ed90f1ea13c8e59e5aa9
Anton Ovchinnikov <anton@tonyo.info>
2022-12-16 16:30:57 +0100

    fix(frontend): Use SENTRY_ENVIRONMENT explicitly on init (#34)

--- src/frontend/sentry.server.config.js
+++ src/frontend/sentry.server.config.js
@@ -8,6 +8,7 @@ Sentry.init({
   dsn: process.env.SENTRY_DSN_SERVER,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
+  environment: process.env.SENTRY_ENVIRONMENT,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
