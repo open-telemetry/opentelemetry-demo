2b1c0c6451603ee94c98572c18b07a6c9e4bd90e
Brett McBride <brett@deakin.edu.au>
2022-12-19 10:56:54 +1100

    update quoteservice to opentelemetry-php beta - update to opentelemetry-php beta versions - import only required opentelemetry-php libraries - use php auto-instrumentation (remove most manual instrumentation, but leave some manual instrumentation to illustrate that auto+manual can work together) - bump php to 8.2, the most recent version supported by opentelemetry-php

--- src/quoteservice/app/routes.php
+++ src/quoteservice/app/routes.php
@@ -1,25 +1,28 @@
 <?php
 declare(strict_types=1);
 
-use OpenTelemetry\API\Trace\AbstractSpan;
+use OpenTelemetry\API\Common\Instrumentation\Globals;
+use OpenTelemetry\API\Trace\Span;
 use OpenTelemetry\API\Trace\SpanKind;
-use OpenTelemetry\SDK\Trace\Tracer;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 
-function calculateQuote($jsonObject, Tracer $tracer): float
+function calculateQuote($jsonObject): float
 {
     $quote = 0.0;
-    $childSpan = $tracer
+    $childSpan = Globals::tracerProvider()->getTracer('manual-instrumentation')
         ->spanBuilder('calculate-quote')
         ->setSpanKind(SpanKind::KIND_INTERNAL)
         ->startSpan();
     $childSpan->addEvent('Calculating quote');
 
     try {
+        if (!array_key_exists('numberOfItems', $jsonObject)) {
+            throw new \InvalidArgumentException('numberOfItems not provided');
+        }
         $numberOfItems = intval($jsonObject['numberOfItems']);
-        $quote = 8.90 * $numberOfItems;
+        $quote = round(8.90 * $numberOfItems, 2);
 
         $childSpan->setAttribute('app.quote.items.count', $numberOfItems);
         $childSpan->setAttribute('app.quote.cost.total', $quote);
@@ -34,14 +37,14 @@ function calculateQuote($jsonObject, Tracer $tracer): float
 }
 
 return function (App $app) {
-    $app->post('/getquote', function (Request $request, Response $response, Tracer $tracer) {
-        $span = AbstractSpan::getCurrent();
+    $app->post('/getquote', function (Request $request, Response $response) {
+        $span = Span::getCurrent();
         $span->addEvent('Received get quote request, processing it');
 
         $body = $request->getBody()->getContents();
         $jsonObject = json_decode($body, true);
 
-        $data = calculateQuote($jsonObject, $tracer);
+        $data = calculateQuote($jsonObject);
 
         $payload = json_encode($data);
         $response->getBody()->write($payload);
