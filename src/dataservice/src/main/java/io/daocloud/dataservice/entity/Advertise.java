fc0436bea4851c0a5075d3dc38062548522be855
Jared Tan <jian.tan@daocloud.io>
2023-01-03 18:34:13 +0800

    add datasource

--- src/dataservice/src/main/java/io/daocloud/dataservice/entity/Advertise.java
+++ src/dataservice/src/main/java/io/daocloud/dataservice/entity/Advertise.java
@@ -1,2 +1,31 @@
-package io.daocloud.dataservice.entity;public class Advertise {
+package io.daocloud.dataservice.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Advertise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(final String content) {
+        this.content = content;
+    }
 }
