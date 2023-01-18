c920a3512a6b42d66f104ad6f25ac3bd9f8336eb
Abhijeet Prasad <aprasad@sentry.io>
2023-01-18 19:16:24 +0100

    feat: Add Sentry Replay to OpenTelemetry demo (#51)

--- src/frontend/sentry.client.config.js
+++ src/frontend/sentry.client.config.js
@@ -9,7 +9,21 @@ Sentry.init({
   environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 1.0,
+
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
   integrations: [
+    new Sentry.Replay({
+      // Additional SDK configuration goes in here, for example:
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
     new Sentry.BrowserTracing({
       _experiments: {
         enableInteractions: true,
