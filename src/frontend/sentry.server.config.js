3cf024b84dfcd42a9af6268575b0b71c021300f9
Abhijeet Prasad <aprasad@sentry.io>
2023-01-23 14:39:03 +0100

    remove node profiling (#63)

--- src/frontend/sentry.server.config.js
+++ src/frontend/sentry.server.config.js
@@ -3,18 +3,14 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
-import '@sentry/tracing';
-import { ProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN_SERVER,
   instrumenter: 'otel',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
-  profilesSampleRate: 1.0,
   includeLocalVariables: true,
   environment: process.env.SENTRY_ENVIRONMENT,
-  integrations: [new ProfilingIntegration()],
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
--- src/paymentservice/sentry.js
+++ src/paymentservice/sentry.js
@@ -1,18 +1,14 @@
 const Sentry = require("@sentry/node");
-require("@sentry/tracing");
-const { ProfilingIntegration } = require("@sentry/profiling-node");
 
 function initSentry() {
   // Make sure to call `Sentry.init` BEFORE initializing the OpenTelemetry SDK
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     tracesSampleRate: 1.0,
-    profilesSampleRate: 1.0,
     // Add local variables to exceptions
     includeLocalVariables: true,
     // set the instrumenter to use OpenTelemetry instead of Sentry
     instrumenter: "otel",
-    integrations: [new ProfilingIntegration()],
     // ...
   });
 }
