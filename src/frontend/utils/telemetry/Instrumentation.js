f5c59ead64b06cdd5227d836c969958e127230af
Oscar Reyes <oscar-rreyes1@hotmail.com>
2023-01-30 19:40:27 -0600

    feature(frontend): adding instrumentation metrics (#632)
    * feature(frontend): adding instrumentation metrics
    
    * feature(frontend): adding instrumentation metrics
    
    * updating package-lock.json
    
    * Update docker-compose.yml
    
    Co-authored-by: Marc Pichler <marcpi@edu.aau.at>
    
    ---------
    
    Co-authored-by: Marc Pichler <marcpi@edu.aau.at>

--- src/frontend/utils/telemetry/Instrumentation.js
+++ src/frontend/utils/telemetry/Instrumentation.js
@@ -1,6 +1,10 @@
 const opentelemetry = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc');
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
+  
+
 const { alibabaCloudEcsDetector } = require('@opentelemetry/resource-detector-alibaba-cloud');
 const { awsEc2Detector, awsEksDetector } = require('@opentelemetry/resource-detector-aws');
 const { containerDetector } = require('@opentelemetry/resource-detector-container');
@@ -9,7 +13,16 @@ const { envDetector, hostDetector, osDetector, processDetector } = require('@ope
 
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: new OTLPTraceExporter(),
-  instrumentations: [getNodeAutoInstrumentations()],
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      '@opentelemetry/instrumentation-fs': {
+        enabled: false,
+      },
+    }),
+  ],
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter(),
+  }),
   resourceDetectors: [
     containerDetector,
     envDetector,
