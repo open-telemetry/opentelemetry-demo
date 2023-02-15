2bc8b10044b2df9dea1c4091560e94898c81daf5
Tomer <4tomer.friedman@gmail.com>
2023-02-14 23:29:53 -0800

    payload

--- src/paymentservice/opentelemetry.js
+++ src/paymentservice/opentelemetry.js
@@ -8,10 +8,15 @@ const { awsEc2Detector, awsEksDetector } = require('@opentelemetry/resource-dete
 const { containerDetector } = require('@opentelemetry/resource-detector-container')
 const { gcpDetector } = require('@opentelemetry/resource-detector-gcp')
 const { envDetector, hostDetector, osDetector, processDetector } = require('@opentelemetry/resources')
+const { httpInstrumentationConfig } = require('./otel-custom/http');
+const { expressInstrumentationConfig } = require('./otel-custom/express');
 
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: new OTLPTraceExporter(),
-  instrumentations: [ getNodeAutoInstrumentations() ],
+  instrumentations: [ getNodeAutoInstrumentations({
+    '@opentelemetry/instrumentation-http': httpInstrumentationConfig,
+    '@opentelemetry/instrumentation-express': expressInstrumentationConfig
+  }) ],
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter()
   }),
---
+++ src/paymentservice/otel-custom/constants.js
@@ -0,0 +1,7 @@
+export const HttpExtendedAttribute = {
+    HTTP_REQUEST_HEADERS: 'http.request.headers',
+    HTTP_REQUEST_BODY:  'http.request.body',
+    HTTP_RESPONSE_HEADERS: 'http.response.headers',
+    HTTP_RESPONSE_BODY: 'http.response.body',
+    HTTP_PATH: 'http.path',
+}
