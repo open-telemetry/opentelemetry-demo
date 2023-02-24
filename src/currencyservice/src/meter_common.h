3a5335a6ea6d56f54b142cc0313e349364da4283
Juliano Costa <julianocosta89@outlook.com>
2023-02-24 21:04:47 +0100

    Fix indentantion

--- src/currencyservice/src/meter_common.h
+++ src/currencyservice/src/meter_common.h
@@ -29,38 +29,38 @@ namespace otlp_exporter = opentelemetry::exporter::otlp;
 
 namespace
 {
-	std::string version{ "1.3.0" };
-	std::string name{ "app_currency" };
-	std::string schema{ "https://opentelemetry.io/schemas/1.2.0" };
+  std::string version{ "1.3.0" };
+  std::string name{ "app_currency" };
+  std::string schema{ "https://opentelemetry.io/schemas/1.2.0" };
 
-	void initMeter() 
-	{
-		//Build MetricExporter
-		otlp_exporter::OtlpGrpcMetricExporterOptions otlpOptions;
+  void initMeter() 
+  {
+    //Build MetricExporter
+    otlp_exporter::OtlpGrpcMetricExporterOptions otlpOptions;
 
-		//Configuration via environment variable not supported yet
-		otlpOptions.endpoint = "otelcol:4317";
-		otlpOptions.aggregation_temporality = metric_sdk::AggregationTemporality::kDelta;
-		auto exporter = otlp_exporter::OtlpGrpcMetricExporterFactory::Create(otlpOptions);
+    //Configuration via environment variable not supported yet
+    otlpOptions.endpoint = "otelcol:4317";
+    otlpOptions.aggregation_temporality = metric_sdk::AggregationTemporality::kDelta;
+    auto exporter = otlp_exporter::OtlpGrpcMetricExporterFactory::Create(otlpOptions);
 
-		//Build MeterProvider and Reader
-		metric_sdk::PeriodicExportingMetricReaderOptions options;
-		options.export_interval_millis = std::chrono::milliseconds(1000);
-		options.export_timeout_millis = std::chrono::milliseconds(500);
-		std::unique_ptr<metric_sdk::MetricReader> reader{
-			new metric_sdk::PeriodicExportingMetricReader(std::move(exporter), options) };
-		auto provider = std::shared_ptr<metrics_api::MeterProvider>(new metric_sdk::MeterProvider());
-		auto p = std::static_pointer_cast<metric_sdk::MeterProvider>(provider);
-		p->AddMetricReader(std::move(reader));
-		metrics_api::Provider::SetMeterProvider(provider);
-	}
+    //Build MeterProvider and Reader
+    metric_sdk::PeriodicExportingMetricReaderOptions options;
+    options.export_interval_millis = std::chrono::milliseconds(1000);
+    options.export_timeout_millis = std::chrono::milliseconds(500);
+    std::unique_ptr<metric_sdk::MetricReader> reader{
+        new metric_sdk::PeriodicExportingMetricReader(std::move(exporter), options) };
+    auto provider = std::shared_ptr<metrics_api::MeterProvider>(new metric_sdk::MeterProvider());
+    auto p = std::static_pointer_cast<metric_sdk::MeterProvider>(provider);
+    p->AddMetricReader(std::move(reader));
+    metrics_api::Provider::SetMeterProvider(provider);
+  }
 
-	nostd::unique_ptr<metrics_api::Counter<uint64_t>> initIntCounter()
-	{
-		std::string counter_name = name + "_counter";
-		auto provider = metrics_api::Provider::GetMeterProvider();
-		nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter(name, version);
-		auto int_counter = meter->CreateUInt64Counter(counter_name);
-		return int_counter;
-	}
+  nostd::unique_ptr<metrics_api::Counter<uint64_t>> initIntCounter()
+  {
+    std::string counter_name = name + "_counter";
+    auto provider = metrics_api::Provider::GetMeterProvider();
+    nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter(name, version);
+    auto int_counter = meter->CreateUInt64Counter(counter_name);
+    return int_counter;
+  }
 }
