9361935e15a47a2cd6fdc6f5dea32bcf896eca27
Pierre Tessier <pierre@pierretessier.com>
2023-03-24 15:54:42 -0400

    fix formatting
    Signed-off-by: Pierre Tessier <pierre@pierretessier.com>

--- src/frontend/utils/telemetry/Instrumentation.js
+++ src/frontend/utils/telemetry/Instrumentation.js
@@ -13,15 +13,15 @@
 // limitations under the License.
 
 const opentelemetry = require('@opentelemetry/sdk-node');
-const {getNodeAutoInstrumentations} = require('@opentelemetry/auto-instrumentations-node');
-const {OTLPTraceExporter} = require('@opentelemetry/exporter-trace-otlp-grpc');
-const {OTLPMetricExporter} = require('@opentelemetry/exporter-metrics-otlp-grpc');
-const {PeriodicExportingMetricReader} = require('@opentelemetry/sdk-metrics');
-const {alibabaCloudEcsDetector} = require('@opentelemetry/resource-detector-alibaba-cloud');
-const {awsEc2Detector, awsEksDetector} = require('@opentelemetry/resource-detector-aws');
-const {containerDetector} = require('@opentelemetry/resource-detector-container');
-const {gcpDetector} = require('@opentelemetry/resource-detector-gcp');
-const {envDetector, hostDetector, osDetector, processDetector} = require('@opentelemetry/resources');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc');
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
+const { alibabaCloudEcsDetector } = require('@opentelemetry/resource-detector-alibaba-cloud');
+const { awsEc2Detector, awsEksDetector } = require('@opentelemetry/resource-detector-aws');
+const { containerDetector } = require('@opentelemetry/resource-detector-container');
+const { gcpDetector } = require('@opentelemetry/resource-detector-gcp');
+const { envDetector, hostDetector, osDetector, processDetector } = require('@opentelemetry/resources');
 
 const sdk = new opentelemetry.NodeSDK({
     traceExporter: new OTLPTraceExporter(),
