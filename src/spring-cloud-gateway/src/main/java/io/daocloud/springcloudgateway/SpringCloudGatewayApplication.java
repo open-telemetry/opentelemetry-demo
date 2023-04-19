58a484f5b2f3bd063bbad345cf14886575dca765
zhuohao.li <zhuohao.li@daocloud.io>
2023-04-19 19:12:19 +0800

    1.upgrade sentinel to 2.0.0-alpha 2.dataservice add h2 database 3.add spring cloud gateway

---
+++ src/spring-cloud-gateway/src/main/java/io/daocloud/springcloudgateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.daocloud.springcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+    }
+
+}
---
+++ src/spring-cloud-gateway/src/test/java/io/daocloud/springcloudgateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.daocloud.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
