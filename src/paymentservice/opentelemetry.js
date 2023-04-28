471204eec5031dffcb241c143822d7e4905abc75
Pierre Tessier <pierre@pierretessier.com>
2023-04-28 10:04:18 -0400

    [frontend] + [paymentservice] - update JS SDKs (#853)
    * update JS SDKs
    
    Signed-off-by: Pierre Tessier <pierre@pierretessier.com>
    
    * only use instrumentation-fs when part of a trace
    
    Signed-off-by: Pierre Tessier <pierre@pierretessier.com>
    
    * update js SDKS
    
    Signed-off-by: Pierre Tessier <pierre@pierretessier.com>
    
    ---------
    
    Signed-off-by: Pierre Tessier <pierre@pierretessier.com>

--- src/paymentservice/opentelemetry.js
+++ src/paymentservice/opentelemetry.js
@@ -2,19 +2,26 @@
 // SPDX-License-Identifier: Apache-2.0
 
 const opentelemetry = require("@opentelemetry/sdk-node")
-const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node")
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc')
-const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc')
-const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics')
-const { alibabaCloudEcsDetector } = require('@opentelemetry/resource-detector-alibaba-cloud')
-const { awsEc2Detector, awsEksDetector } = require('@opentelemetry/resource-detector-aws')
-const { containerDetector } = require('@opentelemetry/resource-detector-container')
-const { gcpDetector } = require('@opentelemetry/resource-detector-gcp')
-const { envDetector, hostDetector, osDetector, processDetector } = require('@opentelemetry/resources')
+const {getNodeAutoInstrumentations} = require("@opentelemetry/auto-instrumentations-node")
+const {OTLPTraceExporter} = require('@opentelemetry/exporter-trace-otlp-grpc')
+const {OTLPMetricExporter} = require('@opentelemetry/exporter-metrics-otlp-grpc')
+const {PeriodicExportingMetricReader} = require('@opentelemetry/sdk-metrics')
+const {alibabaCloudEcsDetector} = require('@opentelemetry/resource-detector-alibaba-cloud')
+const {awsEc2Detector, awsEksDetector} = require('@opentelemetry/resource-detector-aws')
+const {containerDetector} = require('@opentelemetry/resource-detector-container')
+const {gcpDetector} = require('@opentelemetry/resource-detector-gcp')
+const {envDetector, hostDetector, osDetector, processDetector} = require('@opentelemetry/resources')
 
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: new OTLPTraceExporter(),
-  instrumentations: [ getNodeAutoInstrumentations() ],
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      // only instrument fs if it is part of another trace
+      '@opentelemetry/instrumentation-fs': {
+        requireParentSpan: true,
+      },
+    })
+  ],
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter()
   }),
@@ -31,4 +38,4 @@ const sdk = new opentelemetry.NodeSDK({
   ],
 })
 
-sdk.start().then(() => require("./index"));
+sdk.start();
