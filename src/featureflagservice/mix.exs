b8496c0737e221a9f2be5b8efb6bae9fa9570da2
Juliano Costa <juliano.costa@dynatrace.com>
2023-07-21 15:12:26 +0200

    FeatureFlagService-Update dependencies

--- src/featureflagservice/mix.exs
+++ src/featureflagservice/mix.exs
@@ -8,10 +8,10 @@ defmodule Featureflagservice.MixProject do
   def project do
     [
       app: :featureflagservice,
-      version: "0.1.0",
-      elixir: "~> 1.12",
+      version: "1.4.0",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: [] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
@@ -42,27 +42,27 @@ defmodule Featureflagservice.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.9"},
+      {:phoenix, "~> 1.6.16"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, "~> 0.17.2"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
+      {:floki, "~> 0.34.3", only: :test},
+      {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.18"},
-      {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
+      {:gettext, "~> 0.22"},
+      {:jason, "~> 1.4"},
+      {:plug_cowboy, "~> 2.6"},
 
-      {:grpcbox, "~> 0.15.0", override: true},
-      {:opentelemetry_exporter, "~> 1.4.0"},
-      {:opentelemetry_grpcbox, "~> 0.1"},
+      {:grpcbox, "~> 0.16.0", override: true},
+      {:opentelemetry_exporter, "~> 1.6.0"},
+      {:opentelemetry_grpcbox, "~> 0.2"},
       {:opentelemetry_api, "~> 1.2.1"},
-      {:opentelemetry, "~> 1.2.1"},
-      {:opentelemetry_phoenix, "~> 1.0.0"},
+      {:opentelemetry, "~> 1.3.0"},
+      {:opentelemetry_phoenix, "~> 1.1.1"},
       {:opentelemetry_ecto, "~> 1.1.1"}
     ]
   end
