3a0b4efa756d9af9f3af66fa8828b649d37c5482
mackjmr <mackenzie.jomard@datadoghq.com>
2023-07-25 17:59:07 +0200

    Add kafka/{zookeeper,producer,consumer}

--- ci/scripts/ci-deploy-demo.sh
+++ ci/scripts/ci-deploy-demo.sh
@@ -18,6 +18,9 @@ install_demo() {
   # Set the namespace and release name
   release_name="opentelemetry-demo"
 
+  # Deploy zookeeper which is not a default component.
+  kubectl apply -f ./src/zookeeperservice/deployment.yaml -n "${namespace}"
+
   # if repo already exists, helm 3+ will skip
   helm --debug repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 
@@ -25,7 +28,10 @@ install_demo() {
   helm --debug upgrade "${release_name}" -n "${namespace}" open-telemetry/opentelemetry-demo --install \
     -f ./ci/values.yaml \
     --set-string default.image.tag="v$CI_COMMIT_SHORT_SHA"
-
+  
+  # Deploy java order producer which is not a default component.
+  sed -i "s/PLACEHOLDER_COMMIT_SHA/v$CI_COMMIT_SHORT_SHA/g" ./src/orderproducerservice/deployment.yaml
+  kubectl apply -f ./src/orderproducerservice/deployment.yaml -n "${namespace}"
 }
 
 ###########################################################################################################
