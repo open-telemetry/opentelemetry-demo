751bdc890487d3bd157ebb993825df499dfe9e6f
Pierre Tessier <pierre@pierretessier.com>
2023-12-14 22:25:45 -0500

    comments are good
    Signed-off-by: Pierre Tessier <pierre@pierretessier.com>

--- restart-service.sh
+++ restart-service.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script is used to build, then restart the newly built service container. It does this by forcing a full stop
+# and removal of the container, then recreating it. This is useful for development, as it ensures the latest code
+# is running in the container.
+
 if [ -z "$1" ]
 then
     echo "Please provide a service name"
