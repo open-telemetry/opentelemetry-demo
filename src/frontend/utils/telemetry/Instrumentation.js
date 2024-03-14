b3ba547af284b0f2f723a3c0b7cbc4d2027f3a58
Bastian Krol <bastian.krol@web.de>
2024-03-14 09:02:33 +0100

    [dash0] frontend: k8s resource detector again
    This reverts the following commits:
    - 1d52ea49f9f8d00b02ddcef9acb49b2df69ab5ae.
    - 5a45834267f0589752338b3031b67fba9c17eb22

--- src/frontend/utils/telemetry/Instrumentation.js
+++ src/frontend/utils/telemetry/Instrumentation.js
@@ -2,17 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 const opentelemetry = require('@opentelemetry/sdk-node');
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
-const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc');
-const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
-const { alibabaCloudEcsDetector } = require('@opentelemetry/resource-detector-alibaba-cloud');
-const { awsEc2Detector, awsEksDetector } = require('@opentelemetry/resource-detector-aws');
-const { containerDetector } = require('@opentelemetry/resource-detector-container');
-const { gcpDetector } = require('@opentelemetry/resource-detector-gcp');
-const { envDetector, hostDetector, osDetector, processDetector } = require('@opentelemetry/resources');
-
-const KubernetesResourceDetector = require('./KubernetesResourceDetector');
+const {getNodeAutoInstrumentations} = require('@opentelemetry/auto-instrumentations-node');
+const {OTLPTraceExporter} = require('@opentelemetry/exporter-trace-otlp-grpc');
+const {OTLPMetricExporter} = require('@opentelemetry/exporter-metrics-otlp-grpc');
+const {PeriodicExportingMetricReader} = require('@opentelemetry/sdk-metrics');
+const {alibabaCloudEcsDetector} = require('@opentelemetry/resource-detector-alibaba-cloud');
+const {awsEc2Detector, awsEksDetector} = require('@opentelemetry/resource-detector-aws');
+const {containerDetector} = require('@opentelemetry/resource-detector-container');
+const {gcpDetector} = require('@opentelemetry/resource-detector-gcp');
+const {envDetector, hostDetector, osDetector, processDetector} = require('@opentelemetry/resources');
 
 const sdk = new opentelemetry.NodeSDK({
   traceExporter: new OTLPTraceExporter(),
@@ -22,7 +20,7 @@ const sdk = new opentelemetry.NodeSDK({
       '@opentelemetry/instrumentation-fs': {
         requireParentSpan: true,
       },
-    }),
+    })
   ],
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter(),
@@ -37,7 +35,6 @@ const sdk = new opentelemetry.NodeSDK({
     awsEksDetector,
     awsEc2Detector,
     gcpDetector,
-    new KubernetesResourceDetector(),
   ],
 });
 
