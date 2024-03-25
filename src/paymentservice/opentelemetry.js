9165c3a0a43b84a364d81eabbac20eb5008c102f
kimbohlovette <kimbohlovette@gmail.com>
2024-03-25 14:52:45 +0100

    Reformatted the require statements

--- src/paymentservice/opentelemetry.js
+++ src/paymentservice/opentelemetry.js
@@ -2,34 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 const opentelemetry = require('@opentelemetry/sdk-node');
-const {
-	getNodeAutoInstrumentations,
-} = require('@opentelemetry/auto-instrumentations-node');
-const {
-	OTLPTraceExporter,
-} = require('@opentelemetry/exporter-trace-otlp-grpc');
-const {
-	OTLPMetricExporter,
-} = require('@opentelemetry/exporter-metrics-otlp-grpc');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc');
 const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
-const {
-	alibabaCloudEcsDetector,
-} = require('@opentelemetry/resource-detector-alibaba-cloud');
-const {
-	awsEc2Detector,
-	awsEksDetector,
-} = require('@opentelemetry/resource-detector-aws');
-const {
-	containerDetector,
-} = require('@opentelemetry/resource-detector-container');
+const { alibabaCloudEcsDetector } = require('@opentelemetry/resource-detector-alibaba-cloud');
+const { awsEc2Detector, awsEksDetector } = require('@opentelemetry/resource-detector-aws');
+const { containerDetector } = require('@opentelemetry/resource-detector-container');
 const { gcpDetector } = require('@opentelemetry/resource-detector-gcp');
-const {
-	envDetector,
-	hostDetector,
-	osDetector,
-	processDetector,
-} = require('@opentelemetry/resources');
-
+const { envDetector, hostDetector, osDetector, processDetector } = require('@opentelemetry/resources');
 const { PinoInstrumentation } = require('@opentelemetry/instrumentation-pino');
 
 const sdk = new opentelemetry.NodeSDK({
