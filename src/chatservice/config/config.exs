1bc45e170796507400407bf726c16d1fc473dd36
Joshua Lee <josh@joshuamlee.com>
2024-05-12 11:24:10 -0400

    Adds OpenTelemetry to Phoenix
    This commit adds OpenTelemetry instrumentation to Phoenix:
    
    1. Dependencies are added in mix.exs
    2. Configuration options are added in config/config.exs
    3. The OTel instrumentation libraries for Phoenix and ecto are started
    with the application

--- src/chatservice/config/config.exs
+++ src/chatservice/config/config.exs
@@ -61,6 +61,15 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Configure the OpenTelemetry SDK & Exporter
+config :opentelemetry,
+  span_processor: :batch,
+  traces_exporter: :otlp
+
+config :opentelemetry_exporter,
+  otlp_protocol: :http_protobuf,
+  otlp_endpoint: "http://localhost:4318"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
