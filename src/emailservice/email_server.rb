bfc40b96458c0cf03fa474487bb5c695c824a2b1
xuan-cao-swi <xuan.cao@solarwinds.com>
2024-05-15 10:22:57 -0400

    add ruby service

--- src/emailservice/email_server.rb
+++ src/emailservice/email_server.rb
@@ -5,15 +5,9 @@ require "ostruct"
 require "pony"
 require "sinatra"
 
-require "opentelemetry/sdk"
-require "opentelemetry/exporter/otlp"
-require "opentelemetry/instrumentation/sinatra"
+require "solarwinds_apm"
 
 set :port, ENV["EMAIL_SERVICE_PORT"]
-
-OpenTelemetry::SDK.configure do |c|
-  c.use "OpenTelemetry::Instrumentation::Sinatra"
-end
 
 post "/send_order_confirmation" do
   data = JSON.parse(request.body.read, object_class: OpenStruct)
