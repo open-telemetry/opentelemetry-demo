293a22a7acfcbf4300c73cd7ca6d7ab3af542bf9
Joshua Lee <josh@joshuamlee.com>
2024-05-20 22:15:45 -0400

    listen on any interface

--- src/chatservice/config/dev.exs
+++ src/chatservice/config/dev.exs
@@ -19,7 +19,7 @@ config :chatservice, ChatService.Repo,
 config :chatservice, ChatServiceWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
