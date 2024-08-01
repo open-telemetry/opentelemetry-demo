bfb7206ac05363cb0b483f83229637412936edf7
James Lindsay <james.lindsay@smartbear.com>
2024-08-01 09:26:32 +0100

    Set the traceparent header

--- src/frontend/utils/telemetry/BugsnagTracer.ts
+++ src/frontend/utils/telemetry/BugsnagTracer.ts
@@ -1,14 +1,18 @@
 
 
 import BugsnagPerformance from '@bugsnag/browser-performance'
-
 const { BUGSNAG_API_KEY = '' } =
     typeof window !== 'undefined' ? window.ENV : {};
+
 
 const FrontendTracer = async () => {
     BugsnagPerformance.start({
         apiKey: BUGSNAG_API_KEY,
-
+        autoInstrumentNetworkRequests: true,
+        networkRequestCallback: (requestInfo) => {
+            requestInfo.propagateTraceContext = true;
+            return requestInfo;
+        }
     });
 };
 
