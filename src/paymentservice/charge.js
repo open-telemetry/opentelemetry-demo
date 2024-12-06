fc99d6b65a9e1e11cf5e2d6e352677a51dba3a99
Robert Castley <robert.castley@gmail.com>
2024-12-06 18:33:17 +0000

    75% error by version number included tenant.level as span attribute

--- src/paymentservice/charge.js
+++ src/paymentservice/charge.js
@@ -1,24 +1,39 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
-const {context, propagation, trace, metrics} = require('@opentelemetry/api');
+const { context, propagation, trace, metrics } = require('@opentelemetry/api');
 const cardValidator = require('simple-card-validator');
 const { v4: uuidv4 } = require('uuid');
 
 const { OpenFeature } = require('@openfeature/server-sdk');
-const { FlagdProvider} = require('@openfeature/flagd-provider');
+const { FlagdProvider } = require('@openfeature/flagd-provider');
 const flagProvider = new FlagdProvider();
 
 const logger = require('./logger');
 const tracer = trace.getTracer('paymentservice');
 const meter = metrics.getMeter('paymentservice');
-const transactionsCounter = meter.createCounter('app.payment.transactions')
+const transactionsCounter = meter.createCounter('app.payment.transactions');
+
+const TENANT_LEVEL = ['gold', 'silver', 'bronze'];
+
+/** Return random element from given array */
+function random(arr) {
+  const index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
 
 module.exports.charge = async request => {
   const span = tracer.startSpan('charge');
 
   await OpenFeature.setProviderAndWait(flagProvider);
+
   if (await OpenFeature.getClient().getBooleanValue("paymentServiceFailure", false)) {
-    throw new Error("PaymentService Fail Feature Flag Enabled");
+    // 75% chance to fail with version 350.10 span tag
+    if (Math.random() < 0.75) {
+      span.setAttributes({ 'app.version': '350.10', 'app.tenant.level': random(TENANT_LEVEL) });
+      span.end();
+
+      throw new Error('Payment request failed. Invalid token. Version: 350.10');
+    }
   }
 
   const {
@@ -40,29 +55,34 @@ module.exports.charge = async request => {
   });
 
   if (!valid) {
+    span.setAttributes({ 'app.version': '350.09', 'app.tenant.level': random(TENANT_LEVEL) });
     throw new Error('Credit card info is invalid.');
   }
 
   if (!['visa', 'mastercard'].includes(cardType)) {
+    span.setAttributes({ 'app.version': '350.09', 'app.tenant.level': random(TENANT_LEVEL) });
     throw new Error(`Sorry, we cannot process ${cardType} credit cards. Only VISA or MasterCard is accepted.`);
   }
 
   if ((currentYear * 12 + currentMonth) > (year * 12 + month)) {
+    span.setAttributes({ 'app.version': '350.09', 'app.tenant.level': random(TENANT_LEVEL) });
     throw new Error(`The credit card (ending ${lastFourDigits}) expired on ${month}/${year}.`);
   }
 
-  // check baggage for synthetic_request=true, and add charged attribute accordingly
+  // Check baggage for synthetic_request=true, and add charged attribute accordingly
   const baggage = propagation.getBaggage(context.active());
-  if (baggage && baggage.getEntry("synthetic_request") && baggage.getEntry("synthetic_request").value === "true") {
+  if (baggage && baggage.getEntry('synthetic_request') && baggage.getEntry('synthetic_request').value === 'true') {
     span.setAttribute('app.payment.charged', false);
   } else {
     span.setAttribute('app.payment.charged', true);
   }
 
-  span.end();
+  span.setAttributes({ 'app.version': '350.09', 'app.tenant.level': random(TENANT_LEVEL) });
 
   const { units, nanos, currencyCode } = request.amount;
-  logger.info({transactionId, cardType, lastFourDigits, amount: { units, nanos, currencyCode }}, "Transaction complete.");
-  transactionsCounter.add(1, {"app.payment.currency": currencyCode})
-  return { transactionId }
-}
+  logger.info({ transactionId, cardType, lastFourDigits, amount: { units, nanos, currencyCode } }, 'Transaction complete.');
+  transactionsCounter.add(1, { 'app.payment.currency': currencyCode });
+  span.end();
+
+  return { transactionId };
+};
