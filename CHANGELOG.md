9b7cfada48e2ecb39d96fba579202573cbd94c38
Pierre Tessier <pierre@pierretessier.com>
2024-12-16 04:04:24 -0500

    [otel-collector] - rename otelcol to otel-collector (#1841)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -32,6 +32,8 @@ the release.
   ([#1827](https://github.com/open-telemetry/opentelemetry-demo/pull/1827))
 * [cartservice] - Add Exemplars to Cart Service
   ([#1830](https://github.com/open-telemetry/opentelemetry-demo/pull/1830))
+* [otel-collector] rename otelcol to otel-collector
+  ([#1841](https://github.com/open-telemetry/opentelemetry-demo/pull/1841))
 
 ## 1.12.0
 
--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -4,7 +4,6 @@
 import '../styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App, { AppContext, AppProps } from 'next/app';
-import { getCookie } from 'cookies-next';
 import CurrencyProvider from '../providers/Currency.provider';
 import CartProvider from '../providers/Cart.provider';
 import { ThemeProvider } from 'styled-components';
@@ -26,8 +25,7 @@ declare global {
 }
 
 if (typeof window !== 'undefined') {
-  const collector = getCookie('otelCollectorUrl')?.toString() || '';
-  FrontendTracer(collector);
+  FrontendTracer();
   if (window.location) {
     const session = SessionGateway.getSession();
 
