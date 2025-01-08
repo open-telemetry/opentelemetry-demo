1efe410e6454c0ad083d16b0f9066d5882567adf
Charlie Le <charlie_le@apple.com>
2025-01-08 07:22:34 -0800

    Generate proto for C++ (#1871)
    * Generate proto for C++
    
    https://github.com/open-telemetry/opentelemetry-demo/issues/1787
    
    Signed-off-by: Charlie Le <charlie_le@apple.com>
    
    * build: update .licenserc.json to include currency generated files and load environment variables in docker-gen-proto.sh
    
    Signed-off-by: Charlie Le <charlie_le@apple.com>
    
    * fix: update script to source environment variables from the correct path
    
    Signed-off-by: Charlie Le <charlie_le@apple.com>
    
    * build: update Dockerfiles and CMakeLists for currency service, adjust generated files handling
    
    Signed-off-by: Charlie Le <charlie_le@apple.com>
    
    * refactor: rename currency service project to currency-proto and update related CMake configurations
    
    Signed-off-by: Charlie Le <charlie_le@apple.com>
    
    * add line break
    
    ---------
    
    Signed-off-by: Charlie Le <charlie_le@apple.com>
    Co-authored-by: Juliano Costa <julianocosta89@outlook.com>
    Co-authored-by: Juliano Costa <juliano.costa@datadoghq.com>

--- docker-gen-proto.sh
+++ docker-gen-proto.sh
@@ -7,11 +7,20 @@ set -x # Print commands and their arguments as they are executed
 
 # This script is used to generate protobuf files for all services with Docker.
 
+. ./.env
+
 gen_proto_go() {
   echo "Generating Go protobuf files for $1"
   docker build -f "src/$1/genproto/Dockerfile" -t "$1-genproto" .
   docker run --rm -v $(pwd):/build "$1-genproto" \
     protoc -I /build/pb /build/pb/demo.proto --go_out="./src/$1/" --go-grpc_out="./src/$1/"
+}
+
+gen_proto_cpp() {
+  echo "Generating Cpp protobuf files for $1"
+  docker build --build-arg OPENTELEMETRY_CPP_VERSION=${OPENTELEMETRY_CPP_VERSION} -f "src/$1/genproto/Dockerfile" -t "$1-genproto" .
+  docker run --rm -v $(pwd):/build "$1-genproto" \
+    cp -r "/$1/build/generated" "/build/src/$1/build/"
 }
 
 gen_proto_python() {
@@ -21,15 +30,19 @@ gen_proto_python() {
     python -m grpc_tools.protoc -I /build/pb/ --python_out="./src/$1/" --grpc_python_out="./src/$1/" /build/pb/demo.proto
 }
 
-#gen_proto_dotnet accounting
-#gen_proto_java ad
-#gen_proto_dotnet cart
-gen_proto_go checkoutservice
-#gen_proto_cpp currency
-#gen_proto_ruby email
-#gen_proto_ts frontend
-#gen_proto_js payment
-gen_proto_go productcatalogservice
-#gen_proto_php quote
-gen_proto_python recommendation
-#gen_proto_rust shipping
+if [ -z "$1" ]; then
+  #gen_proto_dotnet accounting
+  #gen_proto_java ad
+  #gen_proto_dotnet cart
+  gen_proto_go checkoutservice
+  gen_proto_cpp currency
+  #gen_proto_ruby email
+  #gen_proto_ts frontend
+  #gen_proto_js payment
+  gen_proto_go productcatalogservice
+  #gen_proto_php quote
+  gen_proto_python recommendation
+  #gen_proto_rust shipping
+else
+  "gen_proto_$1" "$2"
+fi
