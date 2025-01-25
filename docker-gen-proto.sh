a544937ed502a95c36f53b64491db16646ea0147
Sozhan Natarajan <sozhan308@gmail.com>
2025-01-25 23:48:50 +0530

    Generate protobuf code for Typescript service - Frontend

--- docker-gen-proto.sh
+++ docker-gen-proto.sh
@@ -30,6 +30,12 @@ gen_proto_python() {
     python -m grpc_tools.protoc -I /build/pb/ --python_out="./src/$1/" --grpc_python_out="./src/$1/" /build/pb/demo.proto
 }
 
+gen_proto_ts() {
+  echo "Generating Typescript protobuf files for $1"
+  docker build -f "src/$1/genproto/Dockerfile" -t "$1-genproto" .
+  docker run --rm -v $(pwd):/build "$1-genproto"
+}
+
 if [ -z "$1" ]; then
   #gen_proto_dotnet accounting
   #gen_proto_java ad
@@ -37,7 +43,7 @@ if [ -z "$1" ]; then
   gen_proto_go checkout
   gen_proto_cpp currency
   #gen_proto_ruby email
-  #gen_proto_ts frontend
+  gen_proto_ts frontend
   #gen_proto_js payment
   gen_proto_go product-catalog
   #gen_proto_php quote
