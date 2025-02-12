62a2c398d0470d0da90eaeb2b66acc9c117bc554
Pierre Tessier <pierre@pierretessier.com>
2025-02-12 03:50:15 -0500

    [frontend] fix build warnings (#2025)
    * upgrade to node 22
    
    * fix build warnings
    
    * fix build warnings
    
    ---------
    
    Co-authored-by: Juliano Costa <julianocosta89@outlook.com>

--- src/frontend/pages/product/[productId]/index.tsx
+++ src/frontend/pages/product/[productId]/index.tsx
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { NextPage } from 'next';
+import Head from 'next/head';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useCallback, useState, useEffect } from 'react';
@@ -64,6 +65,9 @@ const ProductDetail: NextPage = () => {
       productIds={[productId, ...items.map(({ productId }) => productId)]}
       contextKeys={[...new Set(categories)]}
     >
+      <Head>
+        <title>Otel Demo - Product</title>
+      </Head>
       <Layout>
         <S.ProductDetail data-cy={CypressFields.ProductDetail}>
           <S.Container>
