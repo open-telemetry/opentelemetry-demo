abc27ca717db38ff813d3be2b2b36ce9cbeb8247
Gerard Vanloo <gerard.vanloo@ibm.com>
2025-05-29 14:44:18 -0400

    [flagd-ui]: replace alpine image with distroless image
    Signed-off-by: Gerard Vanloo <gerard.vanloo@ibm.com>

--- src/flagd-ui/next.config.mjs
+++ src/flagd-ui/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   basePath: "/feature",
+  reactStrictMode: true,
+  output: 'standalone',
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
