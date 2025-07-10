a51b742f734c17f11008f6e95bb5296f8e7618a8
Alessio Biancalana <alessio@dottorblaster.it>
2025-07-10 18:07:32 +0200

    [flagd-ui] add Credo

--- src/flagd-ui/mix.exs
+++ src/flagd-ui/mix.exs
@@ -55,7 +55,8 @@ defmodule FlagdUi.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
