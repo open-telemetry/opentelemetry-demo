346c879e68e41015ff470787a3c0dc6b6f2a32a8
svrnm <sneumann@causely.ai>
2025-07-22 11:39:08 +0200

    Add Causely to vendors featuring the Astronomy Shop
    Signed-off-by: svrnm <sneumann@causely.ai>

---
+++ src/flagd-ui/src/app/scheduled/page.tsx
@@ -0,0 +1,13 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+"use client";
+import React from "react";
+import ScheduledView from "../../components/scheduled/ScheduledView";
+
+export default function ScheduledPage() {
+  return (
+    <div className="app">
+      <ScheduledView />
+    </div>
+  );
+}
