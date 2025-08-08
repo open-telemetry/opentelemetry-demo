5e49daaa85032d97858c3476c80c0aca8964041d
Alessio Biancalana <alessio@dottorblaster.it>
2025-08-08 12:34:28 +0200

    [flagd-ui] make liveview connect to namespaced path

--- src/flagd-ui/assets/js/app.js
+++ src/flagd-ui/assets/js/app.js
@@ -25,7 +25,7 @@ import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-const liveSocket = new LiveSocket("/live", Socket, {
+const liveSocket = new LiveSocket("/feature/live", Socket, {
   longPollFallbackMs: 2500,
   params: {_csrf_token: csrfToken}
 })
