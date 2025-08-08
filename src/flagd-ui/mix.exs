3584ae8806b6f835f454634432f50907ec006934
Alessio Biancalana <alessio@dottorblaster.it>
2025-08-08 11:48:47 +0200

    [flagd-ui] bump deps for flagd_ui

--- src/flagd-ui/mix.exs
+++ src/flagd-ui/mix.exs
@@ -33,10 +33,10 @@ defmodule FlagdUi.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.8.0-rc.4", override: true},
+      {:phoenix, "~> 1.8.0", override: true},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.0"},
+      {:phoenix_live_view, "~> 1.1"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.9", runtime: Mix.env() == :dev},
