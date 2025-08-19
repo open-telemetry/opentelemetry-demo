6fcfef5728cad477d99b3b9b4fec3536dc0b761a
Alessio Biancalana <alessio@dottorblaster.it>
2025-08-19 14:10:08 +0200

    [flagd-ui] update dev config with path

--- src/flagd-ui/config/dev.exs
+++ src/flagd-ui/config/dev.exs
@@ -9,6 +9,7 @@ import Config
 config :flagd_ui, FlagdUiWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
+  url: [path: "/"],
   http: [ip: {127, 0, 0, 1}, port: 4000],
   check_origin: false,
   code_reloader: true,
