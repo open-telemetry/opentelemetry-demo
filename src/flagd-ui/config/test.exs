7e348c2aa1054cebfd34ea9b0eabc1ea16ee85db
Alessio Biancalana <alessio@dottorblaster.it>
2025-08-19 14:09:49 +0200

    [flagd-ui] update test config

--- src/flagd-ui/config/test.exs
+++ src/flagd-ui/config/test.exs
@@ -3,6 +3,7 @@ import Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :flagd_ui, FlagdUiWeb.Endpoint,
+  url: [path: "/"],
   http: [ip: {127, 0, 0, 1}, port: 4002],
   secret_key_base: "7WKq9+4aRwQEUAE69h03WLZKqkUmgMOlrrXvsq28ZvKNK9HSinOQ3ZBcnUG/eSgb",
   server: false
@@ -22,3 +23,5 @@ config :phoenix, :plug_init_mode, :runtime
 # Enable helpful, but potentially expensive runtime checks
 config :phoenix_live_view,
   enable_expensive_runtime_checks: true
+
+config :flagd_ui, storage_file_path: "./test/fixtures/demo.flagd.json"
