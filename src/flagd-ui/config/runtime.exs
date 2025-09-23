6e6c4836345f6611b8b9cbc660a5c7b191401ae9
Juliano Costa <juliano.costa@datadoghq.com>
2025-09-23 18:11:12 +0200

    [chore] Align env vars

--- src/flagd-ui/config/runtime.exs
+++ src/flagd-ui/config/runtime.exs
@@ -43,7 +43,7 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
+  port = String.to_integer(System.get_env("FLAGD_UI_PORT") || "4000")
 
   config :flagd_ui, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
