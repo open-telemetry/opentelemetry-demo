eb02d551065765b6afd17f74409978b2eca3bccc
dmitchsplunk <derekmitchell@splunk.com>
2025-10-06 15:02:53 -0700

    added skeleton version of the product review service

---
+++ src/product-reviews/product_reviews_server.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+
+# Copyright The OpenTelemetry Authors
+# SPDX-License-Identifier: Apache-2.0
+
+
+# Python
+import os
+import random
+from concurrent import futures
+
+# Pip
+import grpc
+from opentelemetry import trace, metrics
+from opentelemetry._logs import set_logger_provider
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import (
+    OTLPLogExporter,
+)
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.sdk.resources import Resource
+
+# Local
+import logging
+import demo_pb2
+import demo_pb2_grpc
+from grpc_health.v1 import health_pb2
+from grpc_health.v1 import health_pb2_grpc
+
+class ProductReviewService(demo_pb2_grpc.ProductReviewServiceServicer):
+    def GetProductReviews(self, request, context):
+        logger.info(f"Receive GetProductReviews for product id:{request.product_id}")
+        product_reviews = get_product_reviews(request.product_id)
+
+        return product_reviews
+
+    def GetProductReviewSummary(self, request, context):
+        logger.info(f"Receive GetProductReviewSummary for product id:{request.product_id}")
+        product_review_summary = get_product_review_summary(request.product_id)
+
+        return product_review_summary
+
+    def Check(self, request, context):
+        return health_pb2.HealthCheckResponse(
+            status=health_pb2.HealthCheckResponse.SERVING)
+
+    def Watch(self, request, context):
+        return health_pb2.HealthCheckResponse(
+            status=health_pb2.HealthCheckResponse.UNIMPLEMENTED)
+
+def get_product_reviews(request_product_id):
+
+    product_reviews = demo_pb2.GetProductReviewsResponse()
+
+    first_review = product_reviews.product_reviews.add()
+    first_review.username = "Tom"
+    first_review.description = "I love this product, 10 out of 10!"
+    first_review.score = 5
+
+    product_reviews.product_reviews.add(
+        username="Joe",
+        description="It's okay, I prefer another brand.",
+        score=3
+    )
+
+    return product_reviews
+
+def get_product_review_summary(request_product_id):
+
+    product_review_summary = demo_pb2.GetProductReviewSummaryResponse()
+    product_review_summary.product_review_summary = "This is an AI-generated summary"
+
+    return product_review_summary
+
+def must_map_env(key: str):
+    value = os.environ.get(key)
+    if value is None:
+        raise Exception(f'{key} environment variable must be set')
+    return value
+
+
+if __name__ == "__main__":
+    service_name = must_map_env('OTEL_SERVICE_NAME')
+
+    # Initialize Traces and Metrics
+    tracer = trace.get_tracer_provider().get_tracer(service_name)
+    meter = metrics.get_meter_provider().get_meter(service_name)
+
+    # Initialize Logs
+    logger_provider = LoggerProvider(
+        resource=Resource.create(
+            {
+                'service.name': service_name,
+            }
+        ),
+    )
+    set_logger_provider(logger_provider)
+    log_exporter = OTLPLogExporter(insecure=True)
+    logger_provider.add_log_record_processor(BatchLogRecordProcessor(log_exporter))
+    handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
+
+    # Attach OTLP handler to logger
+    logger = logging.getLogger('main')
+    logger.addHandler(handler)
+
+    # Create gRPC server
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+    # Add class to gRPC server
+    service = ProductReviewService()
+    demo_pb2_grpc.add_ProductReviewServiceServicer_to_server(service, server)
+    health_pb2_grpc.add_HealthServicer_to_server(service, server)
+
+    # Start server
+    port = must_map_env('PRODUCT_REVIEWS_PORT')
+    server.add_insecure_port(f'[::]:{port}')
+    server.start()
+    logger.info(f'Product reviews service started, listening on port {port}')
+    server.wait_for_termination()
