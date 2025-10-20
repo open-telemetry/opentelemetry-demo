5d66762b62a95f38da20a8c9ebbfb0c4512f1772
Sajko, Krzysztof <krzysztof.sajko@dynatrace.com>
2025-10-20 12:52:33 +0200

    fix: remove tha changes to payment logging as it was causing crashes

--- src/payment/logger.js
+++ src/payment/logger.js
@@ -1,39 +1,19 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-const pino = require('pino')
+const pino = require("pino");
 
-const transport = pino.transport({
-  target: 'pino-opentelemetry-transport',
-  options: {
-    logRecordProcessorOptions: [
-      {
-        recordProcessorType: 'batch',
-        exporterOptions: {
-          protocol: 'grpc',
-        },
-      },
-      {
-        recordProcessorType: 'simple',
-        exporterOptions: { protocol: 'console' },
-      },
-    ],
-    loggerName: 'payment-logger',
-    serviceVersion: '1.0.0',
-  },
-})
-
-const logger = pino(transport, {
+const logger = pino({
   mixin() {
     return {
-      'service.name': process.env['OTEL_SERVICE_NAME'],
-    }
+      "service.name": process.env["OTEL_SERVICE_NAME"],
+    };
   },
   formatters: {
     level: (label) => {
-      return { level: label }
+      return { level: label };
     },
   },
-})
+});
 
-module.exports = logger
+module.exports = logger;
