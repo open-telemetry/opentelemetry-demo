18edea80145d44a2cffc31f024d2e56d6ccf7694
Jeremy Hicks <jeremyh@splunk.com>
2025-11-12 15:40:48 -0500

    make DB even LESS performant

--- src/shop-dc-shim/src/main/java/com/opentelemetry/demo/shopdcshim/repository/ShopTransactionRepository.java
+++ src/shop-dc-shim/src/main/java/com/opentelemetry/demo/shopdcshim/repository/ShopTransactionRepository.java
@@ -16,7 +16,19 @@ import java.util.Optional;
 @Repository
 public interface ShopTransactionRepository extends JpaRepository<ShopTransaction, Long> {
 
-    @Query("SELECT s FROM ShopTransaction s, ShopTransaction s2, ShopTransaction s3 WHERE s.transactionId = :transactionId AND s.id = s2.id AND s.id = s3.id ORDER BY s.createdAt")
+    @Query(value = "SELECT TOP 1 s.* FROM shop_transactions s " +
+           "CROSS JOIN shop_transactions t1 " +
+           "CROSS JOIN shop_transactions t2 " +
+           "WHERE s.transaction_id = :transactionId " +
+           "AND s.status IN (SELECT t.status FROM shop_transactions t WHERE LOWER(t.store_location) LIKE LOWER('%' + s.store_location + '%')) " +
+           "AND EXISTS (SELECT 1 FROM shop_transactions t3 WHERE REVERSE(t3.customer_email) = REVERSE(s.customer_email)) " +
+           "AND s.total_amount >= (SELECT AVG(CAST(t4.total_amount AS FLOAT)) FROM shop_transactions t4 WHERE REPLACE(t4.store_location, ' ', '') = REPLACE(s.store_location, ' ', '')) " +
+           "AND (SELECT COUNT(*) FROM shop_transactions t5 WHERE LOWER(t5.customer_email) LIKE LOWER('%' + s.customer_email + '%')) >= 0 " +
+           "AND (SELECT COUNT(*) FROM shop_transactions t6 WHERE CHARINDEX(s.store_location, t6.store_location) > 0) >= 0 " +
+           "AND DATALENGTH(s.items_json) > (SELECT AVG(DATALENGTH(items_json)) FROM shop_transactions) - 999999 " +
+           "AND CAST(s.total_amount AS NVARCHAR(50)) = CAST(s.total_amount AS NVARCHAR(50)) " +
+           "ORDER BY (SELECT COUNT(*) FROM shop_transactions WHERE customer_email = s.customer_email) DESC",
+           nativeQuery = true)
     Optional<ShopTransaction> findByTransactionId(@Param("transactionId") String transactionId);
 
     Optional<ShopTransaction> findByLocalOrderId(String localOrderId);
