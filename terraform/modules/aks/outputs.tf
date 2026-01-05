85aa1c4446e717e6dfcb9a3eb0bae8bfe5f0fa58
Roy Zalta <roy@example.com>
2026-01-05 10:20:07 +0200

    feat(terraform): add AKS cluster module
    - Create Azure Kubernetes Service cluster with autoscaling
    - Configure Azure CNI networking and RBAC
    - Add Log Analytics workspace for monitoring
    - Enable Container Insights solution

---
+++ terraform/modules/aks/outputs.tf
@@ -0,0 +1,67 @@
+# Copyright The OpenTelemetry Authors
+# SPDX-License-Identifier: Apache-2.0
+
+output "cluster_id" {
+  description = "ID of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.main.id
+}
+
+output "cluster_name" {
+  description = "Name of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.main.name
+}
+
+output "cluster_fqdn" {
+  description = "FQDN of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.main.fqdn
+}
+
+output "kube_config_raw" {
+  description = "Raw Kubernetes config for the cluster"
+  value       = azurerm_kubernetes_cluster.main.kube_config_raw
+  sensitive   = true
+}
+
+output "kube_config_host" {
+  description = "Kubernetes API server host"
+  value       = azurerm_kubernetes_cluster.main.kube_config[0].host
+  sensitive   = true
+}
+
+output "kube_config_client_certificate" {
+  description = "Base64 encoded client certificate"
+  value       = azurerm_kubernetes_cluster.main.kube_config[0].client_certificate
+  sensitive   = true
+}
+
+output "kube_config_client_key" {
+  description = "Base64 encoded client key"
+  value       = azurerm_kubernetes_cluster.main.kube_config[0].client_key
+  sensitive   = true
+}
+
+output "kube_config_cluster_ca_certificate" {
+  description = "Base64 encoded cluster CA certificate"
+  value       = azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
+  sensitive   = true
+}
+
+output "kubelet_identity_object_id" {
+  description = "Object ID of the kubelet managed identity"
+  value       = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
+}
+
+output "node_resource_group" {
+  description = "Name of the node resource group"
+  value       = azurerm_kubernetes_cluster.main.node_resource_group
+}
+
+output "oidc_issuer_url" {
+  description = "OIDC issuer URL for workload identity"
+  value       = azurerm_kubernetes_cluster.main.oidc_issuer_url
+}
+
+output "log_analytics_workspace_id" {
+  description = "ID of the Log Analytics workspace"
+  value       = azurerm_log_analytics_workspace.main.id
+}
