52b524b2bf21e89c92ef65bd5f01e2c43e23839a
Jonathan Munz <jpmunz@gmail.com>
2026-04-22 09:00:43 -0400

    revert package.json change, include copyright notice

--- src/react-native-app/eslint.config.js
+++ src/react-native-app/eslint.config.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require("eslint-config-expo/flat");
