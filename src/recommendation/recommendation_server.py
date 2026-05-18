b9730e178f148ca214d46ca670633ce50ce0f08f
fbourgey <bourgeyflorian@gmail.com>
2026-05-18 17:12:07 -0400

    rename app.products.count to demo.product.count

--- src/recommendation/recommendation_server.py
+++ src/recommendation/recommendation_server.py
@@ -95,7 +95,7 @@ def get_product_list(request_product_ids):
             cat_response = product_catalog_stub.ListProducts(demo_pb2.Empty())
             product_ids = [x.id for x in cat_response.products]
 
-        span.set_attribute("app.products.count", len(product_ids))
+        span.set_attribute("demo.product.count", len(product_ids))
 
         # Create a filtered list of products excluding the products received as input
         filtered_products = list(set(product_ids) - set(request_product_ids))
