2b966433e5f9f57e226aa0e4471372d7bdf93d3f
fbourgey <bourgeyflorian@gmail.com>
2026-05-19 09:03:24 -0400

    rename app.products_search.count

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -402,7 +402,7 @@ func (p *productCatalog) SearchProducts(ctx context.Context, req *pb.SearchProdu
 	}
 
 	span.SetAttributes(
-		attribute.Int("app.products_search.count", len(result)),
+		attribute.Int("demo.product.search.count", len(result)),
 	)
 	return &pb.SearchProductsResponse{Results: result}, nil
 }
