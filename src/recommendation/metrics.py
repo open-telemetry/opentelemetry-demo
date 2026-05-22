277c7cfe04c9ca3b5b5c870672a4e7e1a64c2626
fbourgey <bourgeyflorian@gmail.com>
2026-05-22 15:59:37 -0400

    renaming

--- src/recommendation/metrics.py
+++ src/recommendation/metrics.py
@@ -6,12 +6,12 @@
 def init_metrics(meter):
 
     # Recommendations counter
-    app_recommendations_counter = meter.create_counter(
-        'app_recommendations_counter', unit='recommendations', description="Counts the total number of given recommendations"
+    recommendation_requests = meter.create_counter(
+        'demo.recommendation.requests', unit='recommendations', description="Counts the total number of given recommendations"
     )
 
     rec_svc_metrics = {
-        "app_recommendations_counter": app_recommendations_counter,
+        "demo.recommendation.requests": recommendation_requests,
     }
 
     return rec_svc_metrics
