42512de3fb11c1ea59577e9668be2115557b3f5a
Matthew Wimpelberg <120263653+mwimpelberg28@users.noreply.github.com>
2026-06-19 06:36:04 -0400

    Update recommendation_server.py
    Co-authored-by: Cyrille Le Clerc <cyrille@cyrilleleclerc.com>

--- src/recommendation/recommendation_server.py
+++ src/recommendation/recommendation_server.py
@@ -138,7 +138,7 @@ if __name__ == "__main__":
 
     # Initialize Logs
     logger_provider = LoggerProvider(
-        resource=Resource.create({}),
+        resource = Resource.create({}),
     )
     set_logger_provider(logger_provider)
     log_exporter = OTLPLogExporter(insecure=True)
