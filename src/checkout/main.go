634c76e69a964a9f39e47a47f1c837065572be47
Aditya Kumar Nayak <adi.nayak45@gmail.com>
2026-07-10 01:31:14 +0530

    refactor(collector): use synthetic attribute names

--- src/checkout/main.go
+++ src/checkout/main.go
@@ -308,9 +308,9 @@ func (cs *checkout) PlaceOrder(ctx context.Context, req *pb.PlaceOrderRequest) (
 	span.SetAttributes(
 		attribute.String("user.id", req.UserId),
 		attribute.String("demo.user_context.selected_currency", req.UserCurrency),
-		attribute.String("demo.user.email", req.Email),
-		attribute.String("demo.payment.card_number", req.CreditCard.CreditCardNumber),
-		attribute.Int("demo.payment.card_cvv", int(req.CreditCard.CreditCardCvv)),
+		attribute.String("demo.user.fake_email", req.Email),
+		attribute.String("demo.payment.fake_card_number", req.CreditCard.CreditCardNumber),
+		attribute.Int("demo.payment.fake_card_cvv", int(req.CreditCard.CreditCardCvv)),
 	)
 
 	if baggage.FromContext(ctx).Member("synthetic_request").Value() == "true" {
