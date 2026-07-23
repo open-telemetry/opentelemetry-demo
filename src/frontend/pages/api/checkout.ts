a33febd22588b0884b1235a22cae1fdf627c14c3
Aditya Kumar Nayak <adi.nayak45@gmail.com>
2026-07-23 16:29:58 +0530

    fix(frontend): address checkout error handling review feedback

--- src/frontend/pages/api/checkout.ts
+++ src/frontend/pages/api/checkout.ts
@@ -3,6 +3,7 @@
 
 import type { NextApiRequest, NextApiResponse } from 'next';
 import type { ServiceError } from '@grpc/grpc-js';
+import { context, Exception, SpanStatusCode, trace } from '@opentelemetry/api';
 import InstrumentationMiddleware from '../../utils/telemetry/InstrumentationMiddleware';
 import CheckoutGateway from '../../gateways/rpc/Checkout.gateway';
 import { Empty, PlaceOrderRequest } from '../../protos/demo';
@@ -17,30 +18,37 @@ const handler = async ({ method, body, query }: NextApiRequest, res: NextApiResp
       const { currencyCode = '' } = query;
       const orderData = body as PlaceOrderRequest;
 
+      let placeOrderResponse;
       try {
-        const { order: { items = [], ...order } = {} } = await CheckoutGateway.placeOrder(orderData);
-
-        const productList: IProductCheckoutItem[] = await Promise.all(
-          items.map(async ({ item: { productId = '', quantity = 0 } = {}, cost }) => {
-            const product = await ProductCatalogService.getProduct(productId, currencyCode as string);
-
-            return {
-              cost,
-              item: {
-                productId,
-                quantity,
-                product,
-              },
-            };
-          })
-        );
-
-        return res.status(200).json({ ...order, items: productList });
+        placeOrderResponse = await CheckoutGateway.placeOrder(orderData);
       } catch (error) {
+        const span = trace.getSpan(context.active());
+        span?.recordException(error as Exception);
+        span?.setStatus({ code: SpanStatusCode.ERROR });
+
         const message = (error as ServiceError)?.details || (error as Error)?.message || 'Failed to place order.';
 
         return res.status(500).json({ error: message });
       }
+
+      const { order: { items = [], ...order } = {} } = placeOrderResponse;
+
+      const productList: IProductCheckoutItem[] = await Promise.all(
+        items.map(async ({ item: { productId = '', quantity = 0 } = {}, cost }) => {
+          const product = await ProductCatalogService.getProduct(productId, currencyCode as string);
+
+          return {
+            cost,
+            item: {
+              productId,
+              quantity,
+              product,
+            },
+          };
+        })
+      );
+
+      return res.status(200).json({ ...order, items: productList });
     }
 
     default: {
